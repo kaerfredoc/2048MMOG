221e1dc155fe19338f59abe73c7080553eb04f58
Tyler <kaerfredoc@gmail.com>
2016-02-20 10:15:54 -0500

    Initial commit Hello World

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # 2048MMOG
 A multi-user contest version of 2048
+
+**Project setup:**
+
+The build.gradle uses the Gradle shadowJar plugin to assemble the application and all it’s dependencies into a single "fat" jar.
+
+To build the "fat jar"
+`./gradlew shadowJar`
+
+Because the application plugin is being used, you may directly run the application:
+`./gradlew run`
+
+You may also run the fat jar as a standalone runnable jar:
+`java -jar build/libs/gradle-simplest-3.2.1-fat.jar`
+(You can take that jar and run it anywhere there is a Java 8+ JDK. It contains all the dependencies it needs so you don’t need to install Vert.x on the target machine).
+
+Now point your browser at http://localhost:8080
---
+++ src/main/java/org/mmog2048/verticles/MainVerticle.java
@@ -0,0 +1,70 @@
+package org.mmog2048.verticles;
+
+import io.vertx.core.*;
+import io.vertx.core.json.JsonObject;
+import org.mmog2048.utils.MultipleFutures;
+import org.mmog2048.utils.SingleFuture;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainVerticle extends AbstractVerticle {
+  public static final int REDIS_PORT = 8888;
+
+  private List<String> deploymentIds;
+
+  @Override
+  public void init(Vertx vertx, Context context) {
+    super.init(vertx, context);
+    deploymentIds = new ArrayList<String>(2);
+  }
+
+  @Override
+  public void start(Future<Void> future) {
+    vertx.createHttpServer()
+        .requestHandler(req -> req.response().end("Hello World!"))
+        .listen(8080, handler -> {
+          if (handler.succeeded()) {
+            System.out.println("http://localhost:8080/");
+          } else {
+            System.err.println("Failed to listen on port 8080");
+          }
+        });
+
+    SingleFuture singleFuture = new SingleFuture();
+    singleFuture.setHandler(new AsyncResultHandler() {
+      @Override
+      public void handle(Object event) {
+
+      }
+    });
+  }
+
+  @Override
+  public void stop(Future<Void> future) {
+    MultipleFutures futures = new MultipleFutures(future);
+    deploymentIds.forEach(deploymentId -> {
+      futures.add(fut -> {
+        undeploy(deploymentId, fut);
+      });
+    });
+    futures.start();
+  }
+
+  private void undeploy(String deploymentId, Future<Void> future) {
+    vertx.undeploy(deploymentId, res -> {
+      if (res.succeeded()) {
+        future.complete();
+      } else {
+        future.fail(res.cause());
+      }
+    });
+  }
+
+  private static JsonObject redisConfig() {
+    JsonObject config = new JsonObject();
+    config.put("host", "localhost");
+    config.put("port", REDIS_PORT);
+    return config;
+  }
+}
