36e51c58d165a0b667d8552bbd0d34326bed55c4
Darin Pope <darin@planetpope.com>
2016-02-20 18:58:36 -0500

    fix syntax

--- src/main/js/board-engine-verticle.js
+++ src/main/js/board-engine-verticle.js
@@ -1,5 +1,5 @@
-vertx.eventBus().consumer("cbweb.validation", (message) => {
-  console.log(message)
-});
+vertx.eventBus().consumer("cbweb.validation", function(message) {
+  console.log(message);
+})
 
 console.log("Board engine started");
