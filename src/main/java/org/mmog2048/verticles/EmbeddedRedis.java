b27fe162d5693ab91ad4418a904b185b84bf5bcd
Darin Pope <darin@planetpope.com>
2016-02-20 15:03:10 -0500

    add logging to track Redis startup issues

--- src/main/java/org/mmog2048/verticles/EmbeddedRedis.java
+++ src/main/java/org/mmog2048/verticles/EmbeddedRedis.java
@@ -2,11 +2,14 @@ package org.mmog2048.verticles;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import redis.embedded.RedisServer;
 
 import java.io.IOException;
 
 public class EmbeddedRedis extends AbstractVerticle {
+    private static final Logger log = LoggerFactory.getLogger(EmbeddedRedis.class);
     private RedisServer server;
 
     @Override
@@ -14,8 +17,10 @@ public class EmbeddedRedis extends AbstractVerticle {
         try {
             server = new RedisServer(MainVerticle.REDIS_PORT);
             server.start(); // seems to be blocking
+            log.info("Redis started on " + MainVerticle.REDIS_PORT);
             future.complete();
         } catch (IOException ioe) {
+            log.error(ioe.getMessage(),ioe);
             future.fail(ioe);
         }
     }
