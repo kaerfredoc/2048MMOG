36855c1983007712fdf3f852f13679fb0571f8b0
Tyler <kaerfredoc@gmail.com>
2016-02-23 23:20:39 -0500

    Responsive grid

--- client/app/views/TopScoresWidget.jsx
+++ client/app/views/TopScoresWidget.jsx
@@ -4,19 +4,36 @@ import AltContainer from 'alt-container';
 import VertxActions from '../actions/VertxActions';
 import TopGamesStore from '../stores/TopGamesStore';
 import VertxStore from '../stores/VertxStore';
+import AutoResponsive from 'autoresponsive-react';
+
+let style = {
+  height: 100,
+  width: 100,
+  cursor: 'default',
+  color: '#514713',
+  borderRadius: 5,
+  boxShadow: '0 1px 0 rgba(255,255,255,0.5) inset',
+  backgroundColor: '#a28f27',
+  borderColor: '#796b1d',
+  fontSize: '80px',
+  lineHeight: '100px',
+  textAlign: 'center',
+  fontWeight: 'bold',
+  textShadow: '1px 1px 0px #ab9a3c'
+};
 
 var gridItem = {
   display: "inline-block",
   border: "2px solid black",
-  width: "28px",
-  height: "28px",
+  width: "38px",
+  height: "38px",
   margin: "2px",
   textAlign: "center",
   fontSize: "1.1em"
 };
 
 var grid = {
-  display: "block",
+  display: "inline-block",
   textAlign: "center",
   padding: "10px"
 };
@@ -64,14 +81,38 @@ class BoardWrapper extends React.Component {
 }
 
 class WidgetView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemMargin: 10,
+      horizontalDirection: 'left',
+      verticalDirection: 'top',
+      containerHeight: null
+    };
+  }
+
+  getAutoResponsiveProps() {
+    return {
+      horizontalDirection: this.state.horizontalDirection,
+      verticalDirection: this.state.verticalDirection,
+      itemMargin: this.state.itemMargin,
+      containerWidth: this.state.containerWidth || this.props.containerWidth,
+      itemClassName: 'item',
+      containerHeight: this.state.containerHeight,
+      transitionDuration: '.8',
+      transitionTimingFunction: 'easeIn'
+    };
+  }
 
   render() {
     return (
-      <div>
-        {this.props.server.boards.map(function (result, idx) {
-          return <BoardWrapper key={idx} data={result}/>;
-        })}
-      </div>
+    <AutoResponsive style={grid} ref="container" {...this.getAutoResponsiveProps()}>
+      {this.props.server.boards.map(function (result, idx) {
+        return (
+          <BoardWrapper style={style} key={idx} data={result}/>
+        )
+      })}
+    </AutoResponsive>
     );
   }
 }
@@ -87,7 +128,7 @@ export default class TopScoresWidget extends React.Component {
 
   render() {
     return (
-      <div className="ui segment">
+      <div className="row">
         <AltContainer stores={{vertx: VertxStore, server: TopGamesStore}}>
           <WidgetView />
         </AltContainer>
