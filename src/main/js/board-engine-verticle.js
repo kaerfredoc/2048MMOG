5cb79b458f06baed954068aa94d80502cb867259
Tyler <kaerfredoc@gmail.com>
2016-02-23 23:39:38 -0500

    Nested bot listener

--- src/main/js/board-engine-verticle.js
+++ src/main/js/board-engine-verticle.js
@@ -1,10 +1,6 @@
 var eb = vertx.eventBus();
 var gameBot = require('./game-bot');
 
-eb.consumer("org.mmog2048", function(message) {
-  console.log("logging from js " + JSON.stringify(message.body()));
-});
-
 console.log("Board engine started");
 
 var GameManager = require('./game-manager');
@@ -12,10 +8,6 @@ var InputManager = require('./websocket-input-manager');
 var StorageManager = require('./redis-storage-manager');
 
 var gameManager = new GameManager(4, InputManager, StorageManager);
-
-eb.consumer("adam.test", function(message) {
-  console.log("boom " + JSON.stringify(message.body()));
-});
 
 for (var x=0; x<100; x++) {
   gameBot(eb, "Bot" + x.toString());
--- src/main/js/game-bot.js
+++ src/main/js/game-bot.js
@@ -1,43 +1,44 @@
 module.exports = function (eb, botName) {
-  var token;
   //Every x seconds try to move...
   var directions = ['U', 'D', 'L', 'R'];
-  var moveAvailable = true;
   console.log(botName);
 
   eb.send("org.mmog2048:register", {"name": botName}, function (res) {
+    var token;
+    var moveAvailable = true;
+
     if (res) {
       console.log("Received token: " + res.body().token);
       console.log("Received board: " + res.body().board);
       token = res.body().token;
     }
-  });
 
+    var scheduleATurn = function (milliseconds) {
+      setTimeout(function () {
+        nextTurnTime = (Math.random() * 1500) + 500;
+        makeAMove();
+        scheduleATurn(nextTurnTime);
+      }, milliseconds);
+    };
+    scheduleATurn(10);
 
-  var makeAMove = function () {
-    if (moveAvailable && token) {
-      var move = directions[Math.round(Math.random() * 3)];
-      // TODO: Consider any edge cases where this deadlocks
-      moveAvailable = false;
-      console.log('sending move ' + move);
-      eb.send("org.mmog2048:move:" + token, {"move": move}, function (res, err) {
-        moveAvailable = true;
-        if (res) {
-          console.log("Received newA board: " + res.body().board);
-        }
-      });
-    }
-  };
-
-  var scheduleATurn = function (milliseconds) {
-    setTimeout(function () {
-      nextTurnTime = (Math.random() * 1500) + 500;
-      makeAMove();
-      scheduleATurn(nextTurnTime);
-    }, milliseconds);
-  };
-  scheduleATurn(10);
+    var makeAMove = function () {
+      if (moveAvailable && token) {
+        var move = directions[Math.round(Math.random() * 3)];
+        // TODO: Consider any edge cases where this deadlocks
+        moveAvailable = false;
+        console.log('sending move ' + move);
+        eb.send("org.mmog2048:move:" + token, {"move": move}, function (res, err) {
+          moveAvailable = true;
+          if (res) {
+            console.log("Received newA board: " + res.body().board);
+          }
+        });
+      }
+    };
+  })
 };
+
 
 
 
